0eeca9edb74a1f80b38cfe8f931052451f56f62c
pr-olga <frkhorkovaolga@gmail.com>
2019-04-06 09:45:43 +0200

    feat: add web worker

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -1,10 +1,10 @@
 (function(){
   // http://stackoverflow.com/questions/10906734/how-to-upload-image-into-html5-canvas
-  var original;
-  var imageLoader = document.querySelector('#imageLoader');
-  imageLoader.addEventListener('change', handleImage, false);
-  var canvas = document.querySelector('#image');
-  var ctx = canvas.getContext('2d');
+    var original;
+    var imageLoader = document.querySelector('#imageLoader');
+    imageLoader.addEventListener('change', handleImage, false);
+    var canvas = document.querySelector('#image');
+    var ctx = canvas.getContext('2d');
 
   function handleImage(e){
     var reader = new FileReader();
@@ -38,25 +38,28 @@
     var a, b, g, i, imageData, j, length, pixel, r, ref;
     imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
-    toggleButtonsAbledness();
+   // toggleButtonsAbledness();
 
     // Hint! This is where you should post messages to the web worker and
     // receive messages from the web worker.
 
-    length = imageData.data.length / 4;
-    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
-      r = imageData.data[i * 4 + 0];
-      g = imageData.data[i * 4 + 1];
-      b = imageData.data[i * 4 + 2];
-      a = imageData.data[i * 4 + 3];
-      pixel = manipulate(type, r, g, b, a);
-      imageData.data[i * 4 + 0] = pixel[0];
-      imageData.data[i * 4 + 1] = pixel[1];
-      imageData.data[i * 4 + 2] = pixel[2];
-      imageData.data[i * 4 + 3] = pixel[3];
+    var worker = new Worker('./scripts/worker.js');
+    worker.postMessage({'imageData': imageData, 'type': type});
+
+    worker.onmessage = function(e) {
+      toggleButtonsAbledness();
+      var image = e.data;
+      if (image) return ctx.putImageData(e.data, 0, 0);
+      console.log("No manipulated image returned.")
     }
-    toggleButtonsAbledness();
-    return ctx.putImageData(imageData, 0, 0);
+
+    worker.onerror = function(error) {
+      function WorkerException(message) {
+        this.name = "WorkerException";
+        this.message = message;
+      };
+      throw new WorkerException('Worker error.');
+    };
   };
 
   function revertImage() {
